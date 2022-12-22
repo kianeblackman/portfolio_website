d468c6ec714f963ce64e64fbcb637600946a9141
kianeblackman <kianeblackman@gmail.com>
2022-12-22 01:59:03 +0000

    changed approach to background colour change and fixed white line between page and navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import AboutPage from './components/AboutPage/AboutPage.tsx';
-import CodePage from './components/CodePage/CodePage.tsx';
-import DesignPage from './components/DesignPage/DesignPage.tsx';
-import FilmPage from './components/FilmPage/FilmPage.tsx';
+import AboutPage from './pages/AboutPage/AboutPage.tsx';
+import CodePage from './pages/CodePage/CodePage.tsx';
+import DesignPage from './pages/DesignPage/DesignPage.tsx';
+import FilmPage from './pages/FilmPage/FilmPage.tsx';
+import React from 'react';
+import { useState } from 'react';
 import './Styles/App.css';
 
 function App() {
+  const [color, changeColor] = useState('#EF4444');
+
+  document.body.style.backgroundColor = color;
   return (
-    <>
+    <div className="w-full h-screen">
       <nav className="flex mb-4 h-12 sticky top-0">
         <li className="list-none text-center w-1/4 text-4xl bg-red-500">
-          <Link to="/">about</Link>
+          <Link to="about" onClick={() => changeColor('#EF4444')}>
+            about
+          </Link>
         </li>
         <li className="list-none text-center w-1/4 text-4xl bg-yellow-500">
-          <Link to="code">code</Link>
+          <Link to="code" onClick={() => changeColor('#EAB308')}>
+            code
+          </Link>
         </li>
         <li className="list-none text-center items-center w-1/4 text-4xl bg-blue-500">
-          <Link to="design">design</Link>
+          <Link to="design" onClick={() => changeColor('#3B82F6')}>
+            design
+          </Link>
         </li>
         <li className="list-none text-center w-1/4 text-4xl bg-green-500">
-          <Link to="film">film</Link>
+          <Link to="film" onClick={() => changeColor('#22C55E')}>
+            film
+          </Link>
         </li>
       </nav>
       <Routes>
-        <Route path="/" element={<AboutPage />}></Route>
+        <Route path="about" exact element={<AboutPage />}></Route>
         <Route path="code" element={<CodePage />}></Route>
         <Route path="design" element={<DesignPage />}></Route>
         <Route path="film" element={<FilmPage />}></Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/pages/CodePage/CodePage.tsx
+++ src/pages/CodePage/CodePage.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function CodePage() {
   return (
-    <div className="h-screen bg-yellow-500"></div>
+    <div></div>
   )
 }
