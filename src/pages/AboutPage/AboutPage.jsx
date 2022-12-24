72376aad34a8f9def781702f0fe010ba1274236e
kianeblackman <kianeblackman@gmail.com>
2022-12-24 12:48:17 +0000

    tec stack tags added

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -42,7 +42,7 @@ export default function AboutPage() {
           <br></br>
           After setting up a gardening business in January 2022 I was given a
           window in the winter period to set my tools aside and embarked on a
-          journey into code through the School of Code bootcamp. Out of 2000
+          journey into code through the School of Code bootcamp. Out of 2EF4444
           applications, a rigorous interview process and the most intense 16
           weeks of my life I can readily say that I’m feeling more than ready to
           begin a new career as a Software Developer.
@@ -63,39 +63,89 @@ export default function AboutPage() {
         </h3>
         <br></br>
         <h2 className="text-3xl underline">Tec Stack</h2>
-        <h2 className="text-xl">Languages</h2>
-        <div className="flex gap-3 pt-3">
-          <img src="https://img.shields.io/badge/javascript-FEC11A?style=for-the-badge&logo=javascript&logoColor=black"></img>
-          <img src="https://img.shields.io/badge/typescript-2D73BE?style=for-the-badge&logo=typescript&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/html-D94924?style=for-the-badge&logo=html5&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/css-254DE2?style=for-the-badge&logo=css3&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/sql-316289?style=for-the-badge&logo=postgresql&logoColor=white"></img>
+        <h2 className="text-xl pt-3">Languages:</h2>
+        <div className="flex flex-wrap gap-3 pt-3">
+          <div className="flex border border-black text-black">
+            <img src="https://img.shields.io/badge/javascript-FEC11A?style=for-the-badge&logo=javascript&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/typescript-FEC11A?style=for-the-badge&logo=typescript&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/html-FEC11A?style=for-the-badge&logo=html5&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/css-FEC11A?style=for-the-badge&logo=css3&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/sql-FEC11A?style=for-the-badge&logo=postgresql&logoColor=black"></img>
+          </div>
         </div>
-        <h2 className="text-xl">Creative</h2>
-        <div className="flex gap-3 pt-3">
-          <img src="https://img.shields.io/badge/miro-55536C?style=for-the-badge&logo=miro&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/photoshop-24A5FE?style=for-the-badge&logo=adobephotoshop&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/indesign-F12E62?style=for-the-badge&logo=adobeindesign&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/illustrator-F49106?style=for-the-badge&logo=adobeillustrator&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/premierepro-9190F3?style=for-the-badge&logo=adobepremierepro&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/aftereffects-6470F0?style=for-the-badge&logo=adobeaftereffects&logoColor=white"></img>
+        <h2 className="text-xl pt-3">Creative:</h2>
+        <div className="flex flex-wrap gap-3 pt-3">
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/miro-FEC11A?style=for-the-badge&logo=miro&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/figma-FEC11A?style=for-the-badge&logo=figma&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/photoshop-FEC11A?style=for-the-badge&logo=adobephotoshop&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/indesign-FEC11A?style=for-the-badge&logo=adobeindesign&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/illustrator-FEC11A?style=for-the-badge&logo=adobeillustrator&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/premierepro-FEC11A?style=for-the-badge&logo=adobepremierepro&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/aftereffects-FEC11A?style=for-the-badge&logo=adobeaftereffects&logoColor=black"></img>
+          </div>
         </div>
-        <h2 className="text-xl">Other</h2>
-        <div className="flex gap-3 pt-3">
-          <img src="https://img.shields.io/badge/javascript-FEC11A?style=for-the-badge&logo=javascript&logoColor=black"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/javascript-FEC11A?style=for-the-badge&logo=javascript&logoColor=black"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
-          <img src="https://img.shields.io/badge/figma-04C47B?style=for-the-badge&logo=figma&logoColor=white"></img>
+        <h2 className="text-xl pt-3">Other:</h2>
+        <div className="flex flex-wrap gap-3 pt-3">
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/react-FEC11A?style=for-the-badge&logo=react&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/next.js-FEC11A?style=for-the-badge&logo=next.js&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/tailwindcss-FEC11A?style=for-the-badge&logo=tailwindcss&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/node.js-FEC11A?style=for-the-badge&logo=node.js&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/express-FEC11A?style=for-the-badge&logo=express&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/vite-FEC11A?style=for-the-badge&logo=vite&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/npm-FEC11A?style=for-the-badge&logo=npm&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/postman-FEC11A?style=for-the-badge&logo=postman&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/netlify-FEC11A?style=for-the-badge&logo=netlify&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/render-FEC11A?style=for-the-badge&logo=render&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/jest-FEC11A?style=for-the-badge&logo=jest&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/cypress-FEC11A?style=for-the-badge&logo=cypress&logoColor=black"></img>
+          </div>
+          <div className="border border-black">
+            <img src="https://img.shields.io/badge/testinglibrary-FEC11A?style=for-the-badge&logo=testinglibrary&logoColor=black"></img>
+          </div>
         </div>
         <br></br>
         <h2 className="text-3xl underline">Education</h2>
--- src/pages/CodePage/CodePage.jsx
+++ src/pages/CodePage/CodePage.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom';
-import Project1 from './projects/Project1.jsx';
-import Project2 from './projects/Project2.jsx';
-import Project3 from './projects/Project3.jsx';
-import Project4 from './projects/Project4.jsx';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CodePage() {
   return (
     <div>
       <ul className="pt-12">
         <div className="grid grid-cols-1 xs:grid-cols-2 gap-6 w-full p-6">
-        <li className="bg-white h-80"><Link to="project1">Project 1</Link></li>
-        <li className="bg-white h-80"><Link to="project2">Project 2</Link></li></div>
-        <div className="grid grid-cols-1 xs:grid-cols-2 gap-6 w-full px-6 pb-6">
-        <li className="bg-white h-80"><Link to="project3">Project 3</Link></li>
-        <li className="bg-white h-80"><Link to="project4">Project 4</Link></li></div>
+          <li className="bg-white h-80">
+            <Link to="trove">Trove</Link>
+          </li>
+          <li className="bg-white h-80">
+            <Link to="to-do-list">To Do List</Link>
+          </li>
+          <li className="bg-white h-80">
+            <Link to="portfolio">Portfolio</Link>
+          </li>
+        </div>
       </ul>
-      <Routes>
-        <Route path="project1" element={<Project1 />}></Route>
-        <Route path="project2" element={<Project2 />}></Route>
-        <Route path="project3" element={<Project3 />}></Route>
-        <Route path="project4" element={<Project4 />}></Route>
-      </Routes>
     </div>
-  )
+  );
 }
