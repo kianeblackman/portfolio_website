8e0e5f15d942f15c9d27e682e005f0a002ee01a3
kianeblackman <kianeblackman@gmail.com>
2022-12-26 14:24:02 +0000

    mammals moo images added to project page

--- src/pages/DesignPage/projects/MammalsMoo.jsx
+++ src/pages/DesignPage/projects/MammalsMoo.jsx
@@ -9,7 +9,14 @@ export default function MammalsMoo() {
     <div className="p-6 pt-16 w-full xs:w-3/4">
       <h1 className="text-5xl xs:text-7xl">Mammals Moo</h1>
       <h2 className="text-2xl xs:text-3xl">Short story collection...</h2>
+      <img src="/images/mammals-moo-front-back.jpg" />
       <img src="/images/mammals-moo-chicken-end.jpg" />
+      <img src="/images/mammals-moo-pig-page.jpg" />
+      <img src="/images/mammals-moo-chicken-page-two.jpg" />
+      <img src="/images/mammals-moo-chicken-page.jpg" />
+      <img src="/images/mammals-moo-end-story.jpg" />
+      <img src="/images/mammals-moo-end-title.jpg" />
+      <img src="/images/mammals-moo-gif.gif" />
     </div>
   );
 }
