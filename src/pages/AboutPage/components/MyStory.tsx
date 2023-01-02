c4df69a167276a73811012beffc391a1fec460eb
kianeblackman <kianeblackman@gmail.com>
2023-01-02 01:45:19 +0000

    nav bar states changed

--- src/pages/AboutPage/components/MyStory.tsx
+++ src/pages/AboutPage/components/MyStory.tsx
@@ -1,33 +1,44 @@
-import React from 'react'
+import React from 'react';
 
 export default function MyStory() {
   return (
     <div>
-      <h2 className="font-regular pb-3 font-head text-2xl underline underline-offset-auto decoration-wavy">My story</h2>
-        <div className="text-1xl font-body">
-          <p>I've always loved problem solving. It's been the common thread
+      <h2 className="font-regular pb-3 font-head text-2xl underline decoration-wavy underline-offset-auto">
+        My story
+      </h2>
+      {/* <hr className='h-1 w-2/4 rounded bg-black border-0'></hr> */}
+      <div className="text-1xl pt-3 font-body">
+        <p>
+          I've always loved problem solving. It's been the common thread
           throughout all of the different career paths I've taken. I enjoy
           trying things I've never tried before, jumping into projects and
-          taking no issue in making mistakes.</p>
-          <br></br>
-          <p>Here's a GIF of me lituraly jumping into something I'd only been
-          learning for an hour:</p>
-          <br></br>
-          <img src="/images/about-page.gif" className="w-full"/>
-          <br></br>   
-          <p>If you're not convinced by that, in September 2022 I took another jump
+          taking no issue in making mistakes.
+        </p>
+        <br></br>
+        <p>
+          Here's a GIF of me lituraly jumping into something I'd only been
+          learning for an hour:
+        </p>
+        <br></br>
+        <img src="/images/about-page.gif" className="w-full" />
+        <br></br>
+        <p>
+          If you're not convinced by that, in September 2022 I took another jump
           into something that I'd been wanting to tackel for years, coding.
           Throughout my different careers I've been interested in learning how
           to code but lack of time and fear of wasted time was a constant
-          barrier faced.</p>
-          <br></br>
-          <p>After setting up a gardening business in January 2022 I was given a
+          barrier faced.
+        </p>
+        <br></br>
+        <p>
+          After setting up a gardening business in January 2022 I was given a
           window in the winter period to set my tools aside and embarked on a
           journey into code through the School of Code bootcamp. Out of 2000
           applications, a rigorous interview process and the most intense 16
           weeks of my life I can readily say that I'm feeling more than ready to
-          begin a new career as a Software Developer.</p>
-        </div>
+          begin a new career as a Software Developer.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
